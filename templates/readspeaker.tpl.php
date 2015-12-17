c6df07e6d8fc5c848ea8151cfd87ab511dd00f96
Thomas de Graaff <tdgraaff@sp.nl>
2015-12-17 10:58:08 +0100

    Initial commit

---
+++ templates/readspeaker.tpl.php
@@ -0,0 +1,8 @@
+<div id="readspeaker_button1" class="rs_skip rsbtn_sp rs_preserve">
+  <div class="rsbtn_button rspart">
+    <a rel="nofollow" class="rsbtn_play" accesskey="L" title="Laat de tekst voorlezen met ReadSpeaker" href="https://app.readspeaker.com/cgi-bin/rsent?customerid=5359&amp;lang=nl_nl&amp;readid=main&amp;url=<?php global $base_url; print urlencode($base_url . '/' . current_path()); ?>">
+     <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees tekst</span></span>
+     <span class="rsbtn_right rsimg rsplay rspart"></span>
+    </a>
+  </div>
+</div>
